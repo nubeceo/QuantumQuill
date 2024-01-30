94445c7dfbcc167ab9a3ba0bce708126ff3ae559
Deep Pal <112393265+deepcs2004@users.noreply.github.com>
2024-01-30 19:51:34 +0530

    updated

--- src/appwrite/Auth.js
+++ src/appwrite/Auth.js
@@ -1,2 +1,46 @@
 import config from "../config/Config";
+import { Client, ID, Account } from 'appwrite'
 
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(config.APPWRITE_URL)
+            .setProject(config.APPWRITE_PROJECT_ID);
+        this.account = new Account(this.client)
+    }
+
+
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+
+            if (userAccount) {
+                // call login function
+                this.loginAccount({ email, password });
+
+            } else {
+                return userAccount;
+            }
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async loginAccount({ email, password }) {
+        try {
+            return await this.account.createEmailSession(email, password)
+        } catch (error) {
+            throw error;
+        }
+    }
+
+}
+
+const authService = new AuthService();
+
+export default authService
